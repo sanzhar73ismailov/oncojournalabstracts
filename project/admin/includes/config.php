6355310411ec13864fe21f8082eb2a46c44ccccf
sanzhar73ismailov <sanzhar73@gmail.com>
2019-02-04 10:06:35 +0100

    Protection

--- project/admin/includes/config.php
+++ project/admin/includes/config.php
@@ -1,17 +1,17 @@
 <?php
 
-
+/*
 define("HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "oncojournalabstracts");
-
-/*
-define("HOST", "localhost");
-define("DB_USER", "v_7179_biostat");
-define("DB_PASS", "cYVklbOO");
-define("DB_NAME", "v_7179_biostat");
 */
+
+define("HOST", "localhost");
+define("DB_USER", "p-508_abstracts");
+define("DB_PASS", "qweasd111!");
+define("DB_NAME", "p-5080_abstracts");
+
 /* Database and Session prefixes */ 
 define('DB_PREFIX', 'bibl_'); ## Do not edit ! 
 
